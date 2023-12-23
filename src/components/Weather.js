ab45be1477cfaf8dd34eef28df0e9a7dcbca18e1
esarii <125924130+esarii@users.noreply.github.com>
2023-12-24 01:17:29 +0300

    json city list added. responsive grid edited.

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,119 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
 function Weather() {
+
+    const [location, setLocation] = useState('');
+    const [data, setData] = useState([]);
+    const { selectedCity, setSelectedCity } = useState('Muğla');
+    const locationURL = "https://turkiyeapi.cyclic.app/api/v1/provinces";
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(locationURL);
+            const result = await response.json();
+            setData(result);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const handleLocationChange = () => {
+        console.log(location);
+        const cityInfo = data.find((city) => city.name.toLowerCase() === location.toLowerCase());
+
+        console.log(cityInfo);
+        setSelectedCity(cityInfo);
+    }
+
     return (
-        <div className='card'>
-            <div className='card-body'>
-                <div class="container text-center">
-                    <div class="row">
-                        <div class="col-md-8">.col-md-8</div>
-                        <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+        <div className="container box">
+            <div className='row'>
+                <div className='col-lg-6 p-5'>
+
+                    <div className='bg'>
+                        <div className='row p-4'>
+                            <div className="input-group">
+                                <input type="text" className="form-control" placeholder="Enter location" aria-describedby="button-addon2"
+                                    value={location}
+                                    onChange={(e) => setLocation(e.target.value)} />
+                                <button className="btn btn-info" type="button" id="button-addon2" onClick={handleLocationChange}>Search</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    {data && (
+                        <div>
+                            <h2>Fetched Data:</h2>
+                            <pre>{JSON.stringify(data, null, 2)}</pre>
+                        </div>
+                    )}
+
+                    <div className='bg mt-4'>
+                        <div className='p-4'>
+                            {selectedCity && (
+                                <h1 className='text-center'>{selectedCity.name}</h1>
+                            )}
+                        </div>
+
+                        <div className='row d-flex align-items-center '>
+
+                            <div className='col-lg-4 col-sm-4 float-center text-center'>
+                                <img
+                                    alt="Weather Icon"
+                                    className="img-fluid"
+                                />
+                            </div>
+                            <div className="col-lg-4 col-sm-4">
+                                <h1 className='display-1 fw-bold text-center'>°C</h1>
+                            </div>
+                            <div className='col-lg-4 col-sm-4 float-start'>
+                                <h6 className='fs-5 fw-light text-center'>description</h6>
+                                <h6 className='fs-6  text-center'><span className='color1'>yağış</span></h6>
+                                <h6 className='fs-6  text-center'><span className='color1'>nem</span></h6>
+                                <h6 className='fs-6  text-center'><span className='color1'>rüzgar</span></h6></div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='col-lg-6'>
+
+                    <div className='col-md-12 px-5 pt-md-5'>
+                        <div className='row bg'>
+                            <h3 className='p-4 color1 text-uppercase fs-5 fw-bold text-center'>5 Günlük Hava Tahmini</h3>
+                            <div className='row mx-auto d-flex align-items-center justify-content-center'>
+                                <h5 className='color1 col-3 text-start fs-6'>denme</h5>
+                                <div className="col-3"><img alt='weather icon' /></div>
+                                <h6 className='color1 col-3 ps-4 text-start fs-6 fw-light'>ddd</h6>
+                                <h4 className='col-3 fw-bold'> "°"</h4>
+
+                            </div>
+                        </div>
+                        <div className='row my-5 justify-content-center align-items-center'>
+
+                            <div className='col-12 d-flex align-items-center justify-content-end'>
+                                <h6 className=' me-3 ms-2 mt-2'>Dark Mode</h6>
+                                <label className="switch">
+                                    <input type="checkbox" id="mode-switch" />
+                                    <span className="slider"></span>
+                                </label>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
             </div>
+
         </div>
+
+
+
+
     )
 }
 
